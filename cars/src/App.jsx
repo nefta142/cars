b7e32d80e98fe95d882fcee464f3b09178f5324c
neftali <neftaliespinorodriguez@gmail.com>
2026-02-02 17:33:09 +0000

    Comienzo de la web

---
+++ cars/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ cars/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import "./Footer.css";
+function Footer(){
+    <footer>
+        <div class="Main-Footer">
+            <p>&copy; 2026</p>
+        </div>
+    </footer>
+}
+export default Footer
